0d11d61374da35adcb0f4bc7673179c89fe293e9
Karla <isckg93@gmail.cm>
2019-04-24 23:06:44 -0500

    migraciones para formatos 3.5-3.7

---
+++ database/migrations/2019_04_24_221805_create_contratos_convenios_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContratosConveniosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contratos_convenios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('relacion_id')->unsigned();
+            $table->string('denominaciones', 2000);
+            $table->date('fecha_subscripcion');
+            $table->date('periodo_inicio');
+            $table->date('periodo_final');
+            $table->integer('importe');
+            $table->string('observaciones', 1000);
+            $table->timestamps();
+            $table->softDeletes(); 
+            
+            $table->foreign('relacion_id')->references('id')->on('relaciones')->onDelete('cascade');  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contratos_convenios');
+    }
+}
